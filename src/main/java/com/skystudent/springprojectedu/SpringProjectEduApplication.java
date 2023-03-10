8d08ba9cf56522a04f1f008b2bad99167b8fd028
Иван <ivanpaskhin@yandex.ru>
2023-03-10 22:19:25 +0300

    Initial commit

---
+++ src/main/java/com/skystudent/springprojectedu/SpringProjectEduApplication.java
@@ -0,0 +1,13 @@
+package com.skystudent.springprojectedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectEduApplication.class, args);
+    }
+
+}
